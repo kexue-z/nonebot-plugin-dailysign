6565d5fa048534300962f62245b1fd98286a506c
kexue <x@kexue.io>
2022-05-24 00:04:18 +0800

    🚧 添加AT人

--- nonebot_plugin_dailysign/__init__.py
+++ nonebot_plugin_dailysign/__init__.py
@@ -16,4 +16,4 @@ async def _(event: GroupMessageEvent):
     group_id = event.group_id
     logger.opt(colors=True).info(f"群 <y>{group_id}</y> : 用户 <y>{user_id}</y> 签到")
     msg = await get_sign_in(user_id, group_id)
-    await sign.finish(msg)
+    await sign.finish(msg, at_sender=True)
